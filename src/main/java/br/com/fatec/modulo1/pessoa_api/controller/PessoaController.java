cd6a4ce0492b9695e4fd00f7d4b3789741db40fe
bportugal01 <bruno2004sa@gmail.com>
2025-11-28 21:50:28 -0300

    fix(MySQL connection): dependencies and configurations for service and application communication

--- src/main/java/br/com/fatec/modulo1/pessoa_api/controller/PessoaController.java
+++ src/main/java/br/com/fatec/modulo1/pessoa_api/controller/PessoaController.java
@@ -1,10 +1,23 @@
 package br.com.fatec.modulo1.pessoa_api.controller;
 
+import br.com.fatec.modulo1.pessoa_api.model.Pessoa;
+import br.com.fatec.modulo1.pessoa_api.services.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")
 public class PessoaController {
+    private final PessoaService service;
 
+    public PessoaController(PessoaService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/get")
+    public List<Pessoa> getPessoa() { return service.listar(); }
 }
--- src/main/java/br/com/fatec/modulo1/pessoa_api/services/PessoaService.java
+++ src/main/java/br/com/fatec/modulo1/pessoa_api/services/PessoaService.java
@@ -1,7 +1,19 @@
 package br.com.fatec.modulo1.pessoa_api.services;
 
+import br.com.fatec.modulo1.pessoa_api.model.Pessoa;
+import br.com.fatec.modulo1.pessoa_api.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PessoaService {
+    public final PessoaRepository pessoaRepository;
+
+    public PessoaService(PessoaRepository pessoaRepository) {
+        this.pessoaRepository = pessoaRepository;
+    }
+
+    public List<Pessoa> listar() { return pessoaRepository.findAll(); }
 }

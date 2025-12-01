6aaab907449d4d379c32dcb8a8098414215218a1
bportugal01 <bruno2004sa@gmail.com>
2025-12-01 00:09:07 -0300

    feat(graylog): Graylog service implementation

--- src/main/java/br/com/fatec/modulo1/pessoa_api/controller/PessoaController.java
+++ src/main/java/br/com/fatec/modulo1/pessoa_api/controller/PessoaController.java
@@ -3,6 +3,8 @@ package br.com.fatec.modulo1.pessoa_api.controller;
 import br.com.fatec.modulo1.pessoa_api.model.Pessoa;
 import br.com.fatec.modulo1.pessoa_api.services.PessoaService;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +24,19 @@ public class PessoaController {
         this.service = service;
     }
 
-    @GetMapping
+    private static  final Logger log = LoggerFactory.getLogger(PessoaController.class);
+
+    @GetMapping("/hoje")
+    public ResponseEntity<String> test() {
+        log.info("Test endpoint '/test' was called");
+        log.debug("Executing test logic for '/test' endpoint");
+        log.warn("This is a sample warning from the '/test' endpoint");
+
+        String responseMessage = "Test successful! Check Graylog logs for details.";
+        return ResponseEntity.ok(responseMessage);
+    }
+
+    @GetMapping("/listar")
     public ResponseEntity<Page<Pessoa>> listar(
             @RequestParam(defaultValue = "0") int pagina) {
         return ResponseEntity.ok(service.listar(pagina));
